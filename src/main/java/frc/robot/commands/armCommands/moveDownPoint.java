b399b9c92428803d4d80f0f9d688461a874cadca
Mathew Clutter <mathew.clutter@mines.sdsmt.edu>
2023-02-11 14:52:12 -0700

    oopsie daisy i forgar to add moveDownPoint

---
+++ src/main/java/frc/robot/commands/armCommands/moveDownPoint.java
@@ -0,0 +1,20 @@
+package frc.robot.commands.armCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.arm;
+
+public class moveDownPoint extends CommandBase{
+
+    private final arm m_arm;
+
+    public moveDownPoint(arm armPassedIn) {
+        m_arm = armPassedIn;
+        addRequirements(armPassedIn);
+    }
+
+    @Override
+    public void execute() {
+        m_arm.moveDownPoint();
+    }
+    
+}
