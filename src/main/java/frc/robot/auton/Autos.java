cc3e6505ba3269c901dae07ab4aa9d4d24d66532
rapidacceleration-4593 <rapidacceleration4593@gmail.com>
2023-03-23 17:26:28 -0600

    untested making long auto longer

--- src/main/java/frc/robot/auton/Autos.java
+++ src/main/java/frc/robot/auton/Autos.java
@@ -97,7 +97,7 @@ public class Autos extends SequentialCommandGroup {
         // Pass through these two interior waypoints, making an 's' curve path
         List.of(new Translation2d(1.5, 0)),
         // End 3 meters straight ahead of where we started, facing forward
-        new Pose2d(3.5, 0, new Rotation2d(0)),
+        new Pose2d(4, 0, new Rotation2d(0)),
         config);
 
         SwerveControllerCommand farDriveCommand =
